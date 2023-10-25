5130b01fb2e2318b30d531d5f234a052c83ac1da
ariella9 <ai396@georgetown.edu>
2023-10-25 18:21:27 -0400

    Set up environ variables

--- app/unemployment.py
+++ app/unemployment.py
@@ -3,6 +3,13 @@ from getpass import getpass
 API_KEY = getpass("Please input your AlphaVantage API Key: ")
 
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # go look in the .env file for any env vars
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
 import requests
 import json
 from pprint import pprint
